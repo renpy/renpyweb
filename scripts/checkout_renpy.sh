76eef2b556e26bd241b523995e3f32cce7a8cd2d
Tom Rothamel <tom@rothamel.us>
2019-04-19 23:30:40 -0400

    Automatically download Ren'Py and extract lib directory.

--- scripts/checkout_renpy.sh
+++ scripts/checkout_renpy.sh
@@ -14,3 +14,14 @@ if [ ! -e renpy ] ; then
     git clone git@github.com:renpy/renpy.git renpy
 fi
 
+
+# If renpy/lib is not present, use the nightly build's SDK.
+if [ ! -e renpy/lib ] ; then
+    pushd renpy
+    curl -o renpy-nightly-sdk.tar.bz2 https://nightly.renpy.org/renpy-nightly-sdk.tar.bz2
+    tar xaf renpy-nightly-sdk.tar.bz2
+    mv renpy-*-sdk/lib .
+    rm -Rf renpy-*-sdk
+    rm renpy-nightly-sdk.tar.bz2
+    popd
+fi
