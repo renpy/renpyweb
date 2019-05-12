761aff7f53796896146f6ffba1719b351d7278a3
Sylvain Beucler <beuc@beuc.net>
2019-05-12 16:57:34 +0200

    Move documentation to the scripts

--- scripts/distribute_game.sh
+++ scripts/distribute_game.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This will run Ren'Py to compile and distribute the game. It takes an
+# optional argument, a path to a Ren'Py game's base directory.
+
 set -e
 
 RENPYWEB="$(dirname $(readlink -f $0))/.."
--- scripts/rebuild_for_renpy.sh
+++ scripts/rebuild_for_renpy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# For use in the Ren'Py auto-build process.  Rebuilds Ren'Py and
+# pygame_sdl2, then does a WASM build and an install.
+
 set -e
 
 RENPYWEB="$(dirname $(readlink -f $0))/.."
