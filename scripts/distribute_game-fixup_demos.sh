bd0973d83229fe3147742187af0102f432af0cbf
Sylvain Beucler <beuc@beuc.net>
2019-06-02 17:23:35 +0200

    Automate the_question and tutorial fix-ups

---
+++ scripts/distribute_game-fixup_demos.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# 'the_question' and 'tutorial' are not suitable for distribution, we
+# need to add missing fonts. Also enable console/developer mode.
+# For debugging and showcasing at https://renpy.beuc.net/play/
+
+set -ex
+
+if [ -z "$1" ]; then
+    echo "usage: $0 game.zip"
+fi
+
+workdir=$(mktemp -d)
+unzip -d "$workdir" "$1"
+mkdir -p "$workdir"/launcher/game/fonts
+cp -a renpy/launcher/game/fonts/Na* renpy/launcher/game/fonts/SourceHanSans-Light-Lite.ttf "$workdir"/launcher/game/fonts
+rm -f "$workdir"/game/script_version.txt
+destzip=$(readlink -f "$1")
+rm -f "$destzip"
+(cd "$workdir" && zip -r "$destzip" .)
+echo "'$1' ready."
