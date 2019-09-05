0caa753db68981d496bb747f6e495b91f550fec0
Sylvain Beucler <beuc@beuc.net>
2019-09-05 15:51:12 +0200

    testserver: prevent caching even when webserver doesn't support if-modified-since

--- testserver.py
+++ testserver.py
@@ -2,7 +2,7 @@ import http.server
 
 class MyHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
     def end_headers(self):
-        self.send_header('Cache-Control', 'max-age=0')
+        self.send_header('Cache-Control', 'no-store')
         http.server.SimpleHTTPRequestHandler.end_headers(self)
 
 if __name__ == '__main__':
