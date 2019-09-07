771465044712002cca5f4236872a7c6f186368f7
Sylvain Beucler <beuc@beuc.net>
2019-09-07 12:59:10 +0200

    main: whitelist debugging trick

--- main.c
+++ main.c
@@ -104,7 +104,9 @@ void pyapp_runmain();
 #ifdef ASYNC
 extern void (*emscripten_sdl_async_callback)(Uint32);
 void async_callback(Uint32 ms) {
+  //emscripten_run_script("console.trace('before sleep');");
   emscripten_sleep_with_yield(ms);
+  //emscripten_run_script("console.trace('after sleep');");
 }
 #endif
 
