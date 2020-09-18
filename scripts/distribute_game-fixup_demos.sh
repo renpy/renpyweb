5b08e7e5dd0d35acbc004bc87d9291d8864e2b7b
Sylvain Beucler <beuc@beuc.net>
2020-09-18 19:17:29 +0200

    Drop temporary comment

--- scripts/distribute_game-fixup_demos.sh
+++ scripts/distribute_game-fixup_demos.sh
@@ -14,7 +14,6 @@ fi
 workdir=$(mktemp -d)
 unzip -d "$workdir" "$1"
 mkdir -p "$workdir"/launcher/game/fonts
-#cp -a renpy/launcher/game/fonts/Na* renpy/launcher/game/fonts/SourceHanSansLite.ttf "$workdir"/launcher/game/fonts
 cp -a renpy/launcher/game/fonts/SourceHanSansLite.ttf "$workdir"/launcher/game/fonts
 rm -f "$workdir"/game/script_version.txt
 destzip=$(readlink -f "$1")
