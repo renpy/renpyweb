3165adc48b9c4a2782ba284bad78b76e3353ab79
Sylvain Beucler <beuc@beuc.net>
2021-03-24 17:44:05 +0100

    Refresh installable files list and drop error-prone check

--- scripts/install_in_renpy.sh
+++ scripts/install_in_renpy.sh
@@ -35,10 +35,8 @@ fi
 
 mkdir renpy/web
 
-for i in asmjs.js asmjs.html.mem asmjs.html asmjs.em index.em index.html index.js index.wasm.gz pyapp.data pyapp-data.js pythonhome.data pythonhome-data.js zee.js renpyweb-version.txt; do
-    if [ -e build/t/$i ]; then
-        cp build/t/$i renpy/web/$i
-    fi
+for i in index.html index.js index.wasm pyapp.data pyapp-data.js pythonhome.data pythonhome-data.js renpyweb-version.txt; do
+    cp build/t/$i renpy/web/$i
 done
 
 cp htaccess.txt renpy/web/
